e77e1d6e2579513ea969cf3358d7547694ac10d2
cromared <croma96@hotmail.com>
2017-11-28 14:06:34 -0300

    automated

--- HelloApp/src/main/java/com/euroamerica/app/App.java
+++ HelloApp/src/main/java/com/euroamerica/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World desde Chile otra vez!" );
+        System.out.println( "Hello World desde Chile otra vez automated!" );
     }
 }
